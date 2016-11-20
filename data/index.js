cebbde84bc33267c09bdb1da677dfe12f4e9cf76
Thiago Siqueira <tsiqueira08@yahoo.com>
2016-11-20 12:42:22 -0500

    Adding mongodb to the project and seed database

--- data/index.js
+++ data/index.js
@@ -1,9 +1,38 @@
 (function(data){
 
-    var seedData = require("./seedData")
+    var seedData = require("./seedData");
+    var database = require("./database");
 
     data.getNoteCategories = function(next){
         next(null, seedData.initialNotes)
     };
+
+    function seedDatabase(){
+        database.getDb(function(err, db){
+            if(err){
+                console.log("Failed to seed DB: " + err);
+            } else {
+                //test to see if data exists
+                db.notes.count(function(err, count){
+                    if (err){
+                        console.log("Failed to retrieve database count");
+                    } else {
+                        if (count == 0){
+                            console.log("Seeding the database...");
+                            seedData.initialNotes.forEach(function(item){
+                                db.notes.insert(item, function(err){
+                                    if(err) console.log("failed to insert note into database");
+                                });
+                            });
+                        } else {
+                            console.log("Database already seeded...")
+                        }
+                    }
+                }); 
+            }
+        });
+    }
+
+    seedDatabase();
 
 })(module.exports);
